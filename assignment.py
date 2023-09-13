895632909d019dd2e5291496bccbf76c5dc5087d
bryanltran <bryan.l.tran-1@ou.edu>
2023-09-13 16:40:49 -0500

    Add reverse

--- assignment.py
+++ assignment.py
@@ -16,6 +16,10 @@ def product(arr):
 
     return(product)
 
+def reverse_array(arr):
+    return arr[::-1]
+
+
 def main():
     # Initialize an empty list to store the integers
     integer_list = []
